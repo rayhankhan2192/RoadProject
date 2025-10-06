c7f246df869c85aac33b82a238edc023345ecc61
Rayhan <rayhankhan1266@gmail.com>
2025-10-06 17:19:06 +0600

    initial changes

--- server/server/settings.py
+++ server/server/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://203.190.12.138:5175"
+    "http://localhost:5175",
+    "http://203.190.12.138:5175",
 ]
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "203.190.12.138"]
 
